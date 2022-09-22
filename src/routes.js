e03955b814041ec0aaf02611336c5f1580c81b34
Rafael Elias <rafaelniza2000@hotmail.com>
2022-09-22 00:48:58 -0300

    remove unused About page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import Home from './routes/Home.html';
-import About from './routes/About.html';
 import ConsultedCharacters from './routes/ConsultedCharacters.html';
 import CharacterInfo from './routes/CharacterInfo.html';
 
 export default {
   '/': Home,
-  '/about': About,
   '/consulted-characters': ConsultedCharacters,
   '/characters-info/:id': {
     component: CharacterInfo,
